fb2d54b7704af14650ce464cce1fdd9ae006817c
NathanielSong <ampnattahapat@gmail.com>
2025-06-04 18:34:55 +0700

    update tester

--- test.js
+++ test.js
@@ -31,7 +31,7 @@ var mysticSeeds = [];
 var unique = 0;
 var uniqueSeeds = [];
 
-while (count < 1000000000) { // 1 billion iterations
+while (count < 2000000000000000) { // 2 quadrillion
     const result = await generateRandom();
     count++;
     switch (result.rarity.name) {
@@ -74,7 +74,34 @@ while (count < 1000000000) { // 1 billion iterations
             unique++;
             break;
     }
-    if (count % 100000 === 0) {
+    if (count > 1000000 && count % 1000000 === 0 && (mystic > 0 || unique > 0)) {
+        console.log(`Generated ${count} characters so far...`);
+        console.log(` |_Common: ${common}(${percentage(common, count)}%)`);
+        console.log(` | |_${lastCommon}`);
+        console.log(` |_Uncommon: ${uncommon}(${percentage(uncommon, count)}%)`);
+        console.log(` | |_${lastUncommon}`);
+        console.log(` |_|_Rare: ${rare}(${percentage(rare, count)}%)`);
+        console.log(` | |_${lastRare}`);
+        console.log(` |_Super Rare: ${superRare}(${percentage(superRare, count)}%)`);
+        console.log(` | |_${lastSuperRare}`);
+        console.log(` |_Super Super Rare: ${superSuperRare}(${percentage(superSuperRare, count)}%)`);
+        console.log(` | |_${lastSuperSuperRare}`);
+        console.log(` |_Ultra Rare: ${ultraRare}(${percentage(ultraRare, count)}%)`);
+        console.log(` | |_${lastUltraRare}`);
+        console.log(` |_Epic: ${epic}(${percentage(epic, count, 3)}%)`);
+        console.log(` | |_${lastEpic}`);
+        console.log(` |_Legendary: ${legendary}(${percentage(legendary, count, 5)}%)`);
+        console.log(` | |_${lastLegendary}`);
+        console.log(` |_Mystic: ${mystic}(${percentage(mystic, count, 0)}%)`);
+        for (let i = 0; i < mysticSeeds.length; i++) {
+            console.log(` | |_${mysticSeeds[i]}`);
+        }
+        console.log(` |_Unique: ${unique}(${percentage(unique, count, 0)}%)`);
+        for (let i = 0; i < uniqueSeeds.length; i++) {
+            console.log(` | |_${uniqueSeeds[i]}`);
+        }
+    }
+    else if (count % 100000 === 0) {
         console.log(`Generated ${count} characters so far...`);
         console.log(` |_Common: ${common}(${percentage(common, count)}%)\n |_Uncommon: ${uncommon}(${percentage(uncommon, count)}%)\n |_Rare: ${rare}(${percentage(rare, count)}%)\n |_Super Rare: ${superRare}(${percentage(superRare, count)}%)\n |_Super Super Rare: ${superSuperRare}(${percentage(superSuperRare, count)}%)\n |_Ultra Rare: ${ultraRare}(${percentage(ultraRare, count)}%)\n |_Epic: ${epic}(${percentage(epic, count, 3)}%)\n |_Legendary: ${legendary}(${percentage(legendary, count, 5)}%)\n |_Mystic: ${mystic}(${percentage(mystic, count, 0)}%)\n |_Unique: ${unique}(${percentage(unique, count, 0)}%)`);
     }
